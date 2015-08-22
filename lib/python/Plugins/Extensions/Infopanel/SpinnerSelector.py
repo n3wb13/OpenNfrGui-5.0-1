eca01a70afcecfea5b38612cb208b3844ff0e6c4
schleichdi2 <super-harry@gmx.at>
2015-08-22 15:21:04 +0200

    fix spinner

--- lib/python/Plugins/Extensions/Infopanel/SpinnerSelector.py
+++ lib/python/Plugins/Extensions/Infopanel/SpinnerSelector.py
@@ -4,14 +4,22 @@ from Screens.MessageBox import MessageBox
 from Screens.Standby import TryQuitMainloop
 from Components.config import config
 import os
+import shutil
 
 
 class SpinnerSelector:
 	def __init__(self,session):
 		self.session = session
-		path = "/usr/share/enigma2/SpinnerSelector/"
+		path = "/usr/share/enigma2/Spinner/"
 		dirs = os.listdir(path)
-		dirs.sort()
+		if not dirs:
+			for i in range(64):
+				if (os.path.isfile("/usr/share/enigma2/spinner/wait%d.png"%(i+1))):
+				        if not os.path.exists("/usr/share/enigma2/Spinner/lastused"):
+                                                os.mkdir("/usr/share/enigma2/Spinner/lastused")
+					shutil.copy("/usr/share/enigma2/spinner/wait%d.png"%(i+1), "/usr/share/enigma2/Spinner/lastused/wait%d.png"%(i+1))
+					dirs = os.listdir(path)
+                dirs.sort()
 		menu = []
 		for dir in dirs:
 			p = path + dir
@@ -25,10 +33,10 @@ class SpinnerSelector:
 		for i in range(64):
 			if (os.path.isfile("/usr/share/enigma2/spinner/wait%d.png"%(i+1))):
 				os.system("rm -f /usr/share/enigma2/spinner/wait%d.png"%(i+1))
-			if (os.path.isfile("/usr/share/enigma2/SpinnerSelector/%s/wait%d.png"%(choice,i+1))):
-				os.system("ln -s /usr/share/enigma2/SpinnerSelector/%s/wait%d.png /usr/share/enigma2/spinner/wait%d.png"%(choice,i+1,i+1))
+			if (os.path.isfile("/usr/share/enigma2/Spinner/%s/wait%d.png"%(choice,i+1))):
+				shutil.copy("/usr/share/enigma2/Spinner/%s/wait%d.png"%(choice,i+1),  "/usr/share/enigma2/spinner/wait%d.png"%(i+1)) 
 		self.session.openWithCallback(self.restart,MessageBox,_("GUI needs a restart to apply a new spinner.\nDo you want to restart the GUI now ?"), MessageBox.TYPE_YESNO)
 
 	def restart(self, answer):
 		if answer is True:
-			self.session.open(TryQuitMainloop, 3)
+			self.session.open(TryQuitMainloop, 3) 
